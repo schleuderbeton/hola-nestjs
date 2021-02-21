41511deb972ef5737d871edfec35ec4b957662bf
Michael Buchli <michael.buchli@wuerth-itensis.com>
2021-02-21 22:21:36 +0100

    add dogs service - CRUD

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import {CreateCatDto} from "./dto/create-cat.dto";
+import {UpdateCatDto} from "./dto/update-cat.dto";
 
 @Controller('cats')
 export class CatsController {
     @Post()
-    create(): string {
+    create(@Body() createCatDto: CreateCatDto) {
         return 'This action adds a new cat';
     }
 
+
     @Get()
-    findAll(): string {
-        return 'This action returns all cats';
+    findAll() {
+        return this.findAll();
+    }
+
+    // @Get()
+    // findAll(@Query() query: ListAllEntities) {
+    //     return `This action returns all cats (limit: ${query.limit} items)`;
+    // }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} cat`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+        return `This action updates a #${id} cat`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} cat`;
     }
 }
